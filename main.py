d54a4a11d95019bbbfc3d423fceaf14a0178c80d
xuan0802 <thespring1989@gmail.com>
2018-04-05 02:59:55 +0900

    topo refactor

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 
-from init_topo import initialize_input
+from topos.random_topo import initialize_input
 from algorithms import bandwidth_greedy, zone_aware, avail_greedy
-from ultilities import print_placement
 from evaluation import evaluate
 
 if __name__ == '__main__':
@@ -10,11 +9,11 @@ if __name__ == '__main__':
     placement_result = bandwidth_greedy.run(input_topo)
     print(evaluate(placement_result, input_topo))
     print("------------zone aware---------------")
-    input_topo1 = initialize_input()
+    input_topo1 = dict(input_topo)
     placement_result1 = zone_aware.run(input_topo1)
     print(evaluate(placement_result1, input_topo1))
     print("------------availability greedy-----")
-    input_topo2 = initialize_input()
+    input_topo2 = dict(input_topo)
     placement_result2 = avail_greedy.run(input_topo2)
     print(evaluate(placement_result2, input_topo2))
 
---
+++ topos/random_topo.py
@@ -0,0 +1,94 @@
+from random import *
+
+N_DC = 10
+N_RN = 10
+
+
+def initialize_input():
+    DC = []
+    AZ = {}
+    SV = {}
+    RN = {}
+    C = {}
+    L = {}
+    BW = {}
+    RD = {}
+    BWR = {}
+    BWT = {}
+    Ad = {}
+    Adz = {}
+    Adzs = {}
+
+    # init set of cloud centers, zones, servers, radio nodes
+    for i in range(N_DC):
+        DC.append('dc' + str(i))
+    for d in DC:
+        AZ[d] = []
+        N_AZ = randint(3, 8)
+        for i in range(N_AZ):
+            AZ[d].append('az' + str(i))
+    for d in DC:
+        for z in AZ[d]:
+            SV[(d, z)] = []
+            N_SV = randint(500, 1000)
+            for i in range(N_SV):
+                SV[(d, z)].append('sv' + str(i))
+
+    for i in range(N_RN):
+        RN['dc' + str(i)] = 'rn' + str(i)
+
+    # init resource capacity
+    C_SV = randint(5, 50)
+    for d in DC:
+        C[d] = N_AZ*N_SV*C_SV
+
+    # init resource demand
+    for d in DC:
+        RD[d] = 100
+
+    # init latency matrix
+    for d in DC:
+        for n in RN.values():
+            L[(d, n)] = randint(150, 205)
+
+    # init link bandwidth
+    for d in DC:
+        for d_ in DC:
+            if d != d_:
+                BW[(d, d_)] = randint(0, 1000)
+            else:
+                BW[(d, d_)] = 1000
+
+    # init bandwidth consumption for state replication and transfer
+    for d in DC:
+        BWR[d] = randint(100, 150)
+        for d_ in DC:
+            if d != d_:
+                BWT[(d, d_)] = 200
+            else:
+                BWT[(d, d_)] = 0
+
+    # init availability tree
+    for d in DC:
+        Ad[d] = 0.98 + random()/100
+        for z in AZ[d]:
+            Adz[(d, z)] = 0.98 + random()/100
+            for s in SV[(d, z)]:
+                Adzs[(d, z, s)] = 0.98
+
+    # create input data
+    input = {}
+    input['DC'] = DC
+    input['AZ'] = AZ
+    input['SV'] = SV
+    input['RN'] = RN
+    input['C'] = C
+    input['L'] = L
+    input['BW'] = BW
+    input['BWR'] = BWR
+    input['BWT'] = BWT
+    input['RD'] = RD
+    input['Ad'] = Ad
+    input['Adz'] = Adz
+    input['Adzs'] = Adzs
+    return input
